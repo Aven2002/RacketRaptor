41acf289aeacc04e61a5106fd0fe3ca1cb9e423e
Aven2002 <avending2002@1utar.my>
2023-11-29 15:37:30 +0800

    Bug Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,96 @@
 <template>
   <div>
-    <!-- Bootstrap Navigation Bar -->
-    <nav
-      class="navbar navbar-expand-lg navbar-dark"
-      style="background-color: #3498db"
-    >
-      <div class="container">
-        <a class="navbar-brand" href="#">
+    <!-- Top Bar: ComLogo, Phone Number, Email -->
+    <div class="top-bar">
+      <div class="navbar-brand-container">
+        <div class="com-logo-container">
           <img
             src="./assets/ComLogo.png"
             alt="ComLogo"
             height="35"
             class="d-inline-block align-top"
           />
+        </div>
+        <div class="contact-info-container">
           <span class="brand-text">
             <strong>PHONE</strong> +603 1022 4197
             <strong> EMAIL</strong> enquiry@racketradar.com
           </span>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          :aria-expanded="isDropdownOpen"
-          aria-controls="navbarNav"
-          @click="toggleDropdown"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="collapse navbar-collapse"
-          :class="{ show: isDropdownOpen }"
-          id="navbarNav"
-        >
-          <ul class="navbar-nav ml-auto">
-            <router-link
-              to="/"
-              class="nav-link"
-              @click="hideDropdown"
-              :class="{ 'active-link': $route.path === '/' }"
-            >
-              <i class="fas fa-home"></i> Home
-            </router-link>
-            <router-link
-              to="/racket"
-              class="nav-link"
-              @click="hideDropdown"
-              :class="{ 'active-link': $route.path === '/racket' }"
-            >
-              <i class="fas fa-medal"></i> RACKET
-            </router-link>
-            <router-link
-              to="/shuttlecock"
-              class="nav-link"
-              @click="hideDropdown"
-              :class="{ 'active-link': $route.path === '/shuttlecock' }"
-            >
-              <i class="fas fa-futbol"></i> SHUTTLECOCK
-            </router-link>
-            <router-link
-              to="/shoes"
-              class="nav-link"
-              @click="hideDropdown"
-              :class="{ 'active-link': $route.path === '/shoes' }"
-            >
-              <i class="fas fa-shoe-prints"></i> SHOES
-            </router-link>
-            <router-link
-              to="/accesories"
-              class="nav-link"
-              @click="hideDropdown"
-              :class="{ 'active-link': $route.path === '/accessories' }"
-            >
-              <i class="fas fa-shopping-bag"></i> ACCESSORIES
-            </router-link>
-          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- Second Part: Navigation Links -->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark"
+      style="background-color: #3498db"
+    >
+      <div class="container">
+        <div class="navbar-collapse-container">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            :aria-expanded="isDropdownOpen"
+            aria-controls="navbarNav"
+            @click="toggleDropdown"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse"
+            :class="{ show: isDropdownOpen }"
+            id="navbarNav"
+          >
+            <ul class="navbar-nav ml-auto">
+              <router-link
+                to="/"
+                class="nav-link"
+                @click="hideDropdown"
+                :class="{ 'active-link': $route.path === '/' }"
+              >
+                <i class="fas fa-home"></i> Home
+              </router-link>
+              <router-link
+                to="/racket"
+                class="nav-link"
+                @click="hideDropdown"
+                :class="{ 'active-link': $route.path === '/racket' }"
+              >
+                <i class="fas fa-medal"></i> RACKET
+              </router-link>
+              <router-link
+                to="/shuttlecock"
+                class="nav-link"
+                @click="hideDropdown"
+                :class="{ 'active-link': $route.path === '/shuttlecock' }"
+              >
+                <i class="fas fa-futbol"></i> SHUTTLECOCK
+              </router-link>
+              <router-link
+                to="/shoes"
+                class="nav-link"
+                @click="hideDropdown"
+                :class="{ 'active-link': $route.path === '/shoes' }"
+              >
+                <i class="fas fa-shoe-prints"></i> SHOES
+              </router-link>
+              <router-link
+                to="/accesories"
+                class="nav-link"
+                @click="hideDropdown"
+                :class="{ 'active-link': $route.path === '/accesories' }"
+              >
+                <i class="fas fa-shopping-bag"></i> ACCESSORIES
+              </router-link>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
+
+    <!-- Router View and Footer Section -->
     <router-view @beforeRouteUpdate="hideDropdown" />
-    <!-- Footer Section -->
     <footer
       class="footer mt-auto py-3"
       style="background-color: #2c3e50; color: #ecf0f1; text-align: center"
@@ -110,16 +121,34 @@ export default {
 </script>
 
 <style>
-/* Brand text styling */
+/* Common styles */
 .brand-text {
   font-size: 14px;
-  color: #ecf0f1; /* Light text color */
-  margin-left: 20px;
+  color: #2c3e50; /* Dark text color */
 }
 
-/* Navbar styling */
-.navbar-nav {
-  margin-left: auto;
+/* Navigation bar styling */
+.navbar {
+  background-color: #3498db; /* Navbar background color */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Box shadow for a subtle lift */
+}
+
+.navbar-brand {
+  display: flex;
+  align-items: center;
+}
+
+.com-logo {
+  margin-right: 10px; /* Adjust the margin between ComLogo and text */
+}
+
+.contact-info {
+  font-size: 14px;
+  color: #ecf0f1; /* Light text color */
+}
+
+.navbar-toggler {
+  border: none;
 }
 
 /* Navigation links styling */
@@ -149,10 +178,10 @@ export default {
 
 /* Active link styling with increased specificity */
 .navbar-nav .nav-link.active-link {
-  color: black;
-  text-decoration: none;
+  color: #2c3e50; /* Dark text color */
 }
 
+/* Underline effect for active link */
 .active-link::before {
   content: " ";
   position: absolute;
@@ -169,15 +198,6 @@ export default {
 .active-link:hover::before {
   transform: scaleX(1);
   transform-origin: bottom left;
-}
-
-/* Adjustments to the toggler button */
-.navbar-toggler {
-  border: none;
-}
-
-.navbar-toggler-icon {
-  background-color: transparent !important;
 }
 
 /* Media query for responsiveness */
@@ -206,8 +226,11 @@ export default {
   text-align: left;
 }
 
-/* Font Awesome icons styling */
-.nav-link i {
-  margin-right: 8px;
+/* Footer styling */
+.footer {
+  background-color: #2c3e50;
+  color: #ecf0f1;
+  text-align: center;
+  padding: 15px 0;
 }
 </style>
